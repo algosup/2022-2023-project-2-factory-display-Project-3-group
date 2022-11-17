7dc223afdd8016e0a580647b9dbd93dee6c0bdb7
karinevinette <karine.vinette@algosup.com>
2022-11-17 05:28:46 +0100

    add timer start

---
+++ ecrans/script1.js
@@ -0,0 +1,6 @@
+document.getElementById('start1').innerHTML = 'Ecran1';
+
+setTimeout(function() {
+  document.getElementById('start1').innerHTML = '';
+}, 10000);
+
---
+++ ecrans/script2.js
@@ -0,0 +1,5 @@
+document.getElementById('start2').innerHTML = 'Ecran2';
+
+setTimeout(function() {
+  document.getElementById('start2').innerHTML = '';
+}, 10000);
