e279555fbe20231b3c42527855b5162784588169
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2022-11-29 14:27:34 +0100

    Fix login bug

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,3 @@
-
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
  import { getDatabase, set, ref, update} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
@@ -18,47 +17,26 @@
    appId: "1:78871282404:web:2e851b03a340a732c522f1"
  };
 
- // Initialize Firebase
  const app = initializeApp(firebaseConfig);
  const database = getDatabase(app);
  const auth = getAuth();
- 
-   
-login.addEventListener('click',(e)=>{
-const email = document.getElementById('email2').value;
-const password = document.getElementById('password2').value;
 
-   signInWithEmailAndPassword(auth, email, password)
-   .then((userCredential) => {
-     // Signed in 
-     const user = userCredential.user;
 
-     const dt = new Date();
-      update(ref(database, 'users/' + user.uid),{
-       last_login: dt,
-     })
 
-      alert('User loged in!');
-      window.location = 'accueil.html';
-   })
-   .catch((error) => {
-     const errorCode = error.code;
-     const errorMessage = error.message;
+
+
+logout.addEventListener('click',(e)=>{
+
+signOut(auth).then(() => {
+  // Sign-out successful.
+  alert('user loged out');
+  window.location = 'accueil.html';
+}).catch((error) => {
+  // An error happened.
+  const errorCode = error.code;
+  const errorMessage = error.message;
 
      alert(errorMessage);
 });
 
 });
-const user = auth.currentUser;
-onAuthStateChanged(auth, (user) => {
-if (user) {
- const uid = user.uid;
- // ...
-} else {
- // User is signed out
- // ...
-}
-});
-
-
-
--- js/register.js
+++ js/register.js
@@ -1,4 +1,3 @@
-
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
  import { getDatabase, set, ref, update} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
@@ -18,47 +17,38 @@
    appId: "1:78871282404:web:2e851b03a340a732c522f1"
  };
 
- // Initialize Firebase
  const app = initializeApp(firebaseConfig);
  const database = getDatabase(app);
  const auth = getAuth();
- 
-   
-login.addEventListener('click',(e)=>{
-const email = document.getElementById('email2').value;
-const password = document.getElementById('password2').value;
 
-   signInWithEmailAndPassword(auth, email, password)
-   .then((userCredential) => {
-     // Signed in 
-     const user = userCredential.user;
+signUp.addEventListener('click',(e) => {
 
-     const dt = new Date();
-      update(ref(database, 'users/' + user.uid),{
-       last_login: dt,
-     })
-
-      alert('User loged in!');
-      window.location = 'accueil.html';
-   })
-   .catch((error) => {
-     const errorCode = error.code;
-     const errorMessage = error.message;
-
-     alert(errorMessage);
-});
-
-});
-const user = auth.currentUser;
-onAuthStateChanged(auth, (user) => {
-if (user) {
- const uid = user.uid;
- // ...
-} else {
- // User is signed out
- // ...
-}
-});
-
-
-
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const name = document.getElementById('name').value;
+    const familyname = document.getElementById('familyname').value;
+    
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+       // Signed in 
+        const user = userCredential.user;
+    
+        set(ref(database, 'users/' + user.uid),{
+            name: 'name',
+            familyname: 'familyname',
+            email: 'email'
+        })
+    
+        alert('user created!');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    
+        alert(errorMessage);
+      // ..
+      });
+    
+    });
+    
