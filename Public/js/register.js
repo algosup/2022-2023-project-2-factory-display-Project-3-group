23ed0c5014d593e1771528520555f3dc524eac66
Quentin-Clement <quentin.clement@algosup.com>
2022-12-02 15:41:51 +0100

    update signUp

--- Public/js/register.js
+++ Public/js/register.js
@@ -3,9 +3,6 @@
  import { getDatabase, set, ref, update} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
-
-
-
  // Your web app's Firebase configuration
  const firebaseConfig = {
    apiKey: "AIzaSyDp6LAdcILJ4vgNsUPUAxQQcYKqbP9gGaI",
@@ -25,42 +22,86 @@ signUp.addEventListener('click',(e) => {
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
     const name = document.getElementById('name').value;
     const familyname = document.getElementById('familyname').value;
+    const success = document.getElementById('success');
+    const signupBox = document.getElementById('signup-box');
+    const signup = document.getElementById('signUp');
+
+    if (email.endsWith("@jacobi.fr"))
+    {
+      if (confirmPassword == password) {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
     
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-       // Signed in 
-        const user = userCredential.user;
-    
-        set(ref(database, 'users/' + user.uid),{
+          set(ref(database, 'users/' + user.uid),{
             name: 'name',
             familyname: 'familyname',
             email: 'email'
+          })
+          const successMessage = "Votre compte a été créé avec succès.";
+          var p = document.createElement("p");
+          p.textContent = successMessage;
+          success.innerHTML = "";
+          success.appendChild(p);
+          success.style.color = "green";
+          signupBox.style.height = "130%";
+          signup.style.marginTop = "0px";
+          // document.getElementById("email2").value = "";
+          // document.getElementById("password2").value = "";
+          // ...
+
+          setTimeout(
+            function() {
+              window.location = 'accueil.html';
+            }, 3000);
         })
-        const successMessage = "Votre compte a bien été créé avec succès";
-        var p = document.createElement("p");
-        p.textContent = successMessage;
-        document.getElementById("success").innerHTML = "";
-        document.getElementById("success").appendChild(p);
-        document.getElementById("success").style.fontSize = "90%";
-        document.getElementById("signup-box").style.height = "450px";
-        document.getElementById("signUp").style.marginTop = "0px";
-        // document.getElementById("email2").value = "";
-        // document.getElementById("password2").value = "";
-        // ...
 
-        setTimeout(
-          function() {
-            window.location = 'accueil.html';
-          }, 5000);
-      })
-
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      // ..
+        .catch((error) => {
+          if (error.code == "auth/email-already-in-use") {
+            const errorMessage = "Cette adresse email est déjà utilisée.";
+            var p = document.createElement("p");
+            p.textContent = errorMessage;
+            success.innerHTML = "";
+            success.appendChild(p);
+            success.style.color = "red";
+            signupBox.style.height = "128%";
+            signup.style.marginTop = "0px";
+          } 
       });
-    
-    });
-    
+             
+      } else {
+        const errorMessage = "Les mots de passe ne correspondent pas.";
+        var p = document.createElement("p");
+        p.textContent = errorMessage;
+        success.innerHTML = "";
+        success.appendChild(p);
+        success.style.color = "red";
+        signupBox.style.height = "136%";
+        signup.style.marginTop = "0px";
+      }
+    }
+
+    else {
+      const jacobiMessage = "Veuillez utiliser une adresse mail Jacobi.";
+      var p = document.createElement("p");
+      p.textContent = jacobiMessage;
+      success.innerHTML = "";
+      success.appendChild(p);
+      success.style.color = "red";
+      signupBox.style.height = "136%";
+      signup.style.marginTop = "0px";
+    };
+  });
+
+  function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
