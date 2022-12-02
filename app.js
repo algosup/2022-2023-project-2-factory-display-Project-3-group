3c194307ab8d478747cdd4a1342a8aa2563e6302
karinevinette <karine.vinette@algosup.com>
2022-12-02 19:53:32 +0100

    s

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-import express from 'express';
-import mangoose from 'mongoose';
-import path from 'path';
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mangoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
+var app = express();
+var cons = require('consolidate')
 
 
 mangoose.connect('mongodb://jacobi-test-server:4SWaWGVEAP8lIl1j707x8cv1kIaN6StERsBHBeEYasnfW20lnQgBYKGAH96ooau7u0KLp1S73oVhACDb4mMFRw==@jacobi-test-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@jacobi-test-server', {
@@ -12,21 +19,40 @@ mangoose.connect('mongodb://jacobi-test-server:4SWaWGVEAP8lIl1j707x8cv1kIaN6StER
 
 
 const app = express();
-app.use(express.static('Public'));
 
+app. engine('html', cons.swig)
+// view engine setup
+ app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html'); 
 
-
-    app.get('/', (req, res) => {
+   /*  app.get('/', (req, res) => {
         // res.send('Hello World!')
         const htmlPath = path.resolve('Public/HTML/index.html');
         res.sendFile
         (htmlPath);
-      });
+      }); */
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    }
-);
+      app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
+  
 
 
 
