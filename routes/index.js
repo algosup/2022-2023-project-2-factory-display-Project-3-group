b122b2e183578de6072133c0910da08d65163124
karinevinette <karine.vinette@algosup.com>
2022-12-05 10:26:59 +0100

    add routes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,26 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/accueil.html', function(req, res, next) {
+  res.render('accueil.html');
+});
+router.get('/campaign.html', function(req, res, next) {
+  res.render('campaign.html');
+});
+router.get('/addScreen.html', function(req, res, next) {
+  res.render('addScreen.html');
+});
+router.get('/addVideo.html', function(req, res, next) {
+  res.render('addVideo.html');
+});
+router.get('/newAccount.html', function(req, res, next) {
+  res.render('newAccount.html');
+});
+router.get('/newCampaign.html', function(req, res, next) {
+  res.render('newCampaign.html');
+});
+router.get('/screenInfo.html', function(req, res, next) {
+  res.render('screenInfo.html');
+});
 
 module.exports = router;
