e2b41c65e842426f073298f63faf76868c6322e6
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2022-12-06 10:39:51 +0100

    Implement Campaign Backend page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import './db.js';
-import { Test } from './db.js';
+import { Campaign, Test } from './db.js';
 
 const app = express();
 const port = 3000;
@@ -9,24 +9,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./Public'));
 
-// app.get('/api', (req, res) => {
-//     console.log('Hello World!');
-//     return res.status(401).json('Unauthorized');
-// });
 
-app.post("/api/addname", (req, res) => {
-    const myData = new Test(req.body);
+
+app.post("/api/newCampaign", (req, res) => {
+    const myData = new Campaign(req.body);
     myData.save()
     .then(item => {
-    res.send("item saved to database");
+    res.send("Campaign created");
     })
     .catch(err => {
     res.status(400).send("unable to save to database");
     });
    });
-   
+
+
+
+app.get("/api/getCampaign", (req, res) => {
+    Campaign.find({}, (err, result) => {
+        if (err) {
+            res.status(400).send("unable to load data");
+        } else {
+            return res.json(result);
+        }
+    });
+});
+
+
+
+      
    
 
-app.listen(port, () => {
+
+
+            app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
