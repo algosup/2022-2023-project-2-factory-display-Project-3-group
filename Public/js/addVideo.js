77973290dbdb1150ee8e114d3f20aec0c30759b5
karinevinette <karine.vinette@algosup.com>
2022-12-11 18:47:51 +0100

    popup campagne créée

--- Public/js/addVideo.js
+++ Public/js/addVideo.js
@@ -18,3 +18,14 @@ function readVideo(event) {
     reader.readAsDataURL(event.target.files[0]);
   }
 }
+
+var validBtn = document.getElementById("valider");
+validBtn.addEventListener("click", function () {
+  
+    confirm("Votre campagne a été créée");
+
+    window.location.href = "accueil.html";
+  
+
+});
+
---
+++ Public/js/newcampaign.js
@@ -0,0 +1,10 @@
+var validBtn = document.getElementById("valider");
+validBtn.addEventListener("click", function () {
+  
+    confirm("Votre campagne a été créée");
+
+    window.location.href = "accueil.html";
+  
+
+});
+
